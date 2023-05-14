52abd26015ef2a464bd10b76f76d451f2cb8d416
mohammed <malajaji.97@gmail.com>
2023-05-14 14:40:16 +0300

    all good

--- src/main/java/com/example/spring_homework11/Controller/welcomeController.java
+++ src/main/java/com/example/spring_homework11/Controller/welcomeController.java
@@ -1,2 +1,30 @@
-package com.example.spring_homework11.Controller;public class welcomeController {
+package com.example.spring_homework11.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("get")
+public class welcomeController {
+
+    @GetMapping(path = "/name")
+    public String getName(){
+        return "My name is Mohammed Alajaji";
+    }
+    @GetMapping(path = "/age")
+    public String getAge(){
+        return "I am 25 years old";
+    }
+    @GetMapping(path = "/check/status")
+    public String checkStatus(){
+        return "Everything is OK";
+    }
+    @GetMapping(path = "health")
+    public String getHealth(){
+        return "Server health is up and running";
+    }
+
+
 }
